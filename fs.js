9b0f1ff00656b26793c6e96be8e2e6b67bfb9256
RSamaium <sam@newbot.io>
2024-09-23 15:04:09 +0200

    promise

--- fs.js
+++ fs.js
@@ -1,15 +1,24 @@
 //const fs = require('fs') // cjs
-import fs from 'fs'
+import fs from 'fs/promises'
 //import { add } from './math'
 
-fs.writeFile('./test.txt', 'hello world', () => {
-    console.log('terminé')
-})
+// fs.writeFile('./test.txt', 'hello world', () => {
+//     console.log('terminé')
+// })
 
-fs.readFile('./test.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.log(err.message)
-        return
-    }
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         console.log(err.message)
+//         return
+//     }
+//     console.log(data)
+// })
+
+try {
+    await fs.writeFile('./test.txt', 'hello world')
+    const data = await fs.readFile('./test.txt', 'utf8')
     console.log(data)
-})
+}
+catch (err) {
+    console.log(err)
+}
--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-import http from 'http'
+import http from "http";
+import fs from "fs/promises";
+import path from "path";
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    res.end('<h1>Hello World !</h1>')
-})
+const port = process.argv[2] || 3000;
 
-server.listen(3000, () => {
-    console.log('serveur démarre')
-})
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const server = http.createServer(async (req, res) => {
+  if (req.url == "/") {
+    try {
+      const content = await fs.readFile(
+        path.join(__dirname, "static", "index.html"),
+        "utf8"
+      );
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.end(content);
+    } catch (err) {
+      res.writeHead(500);
+      res.end();
+    }
+  } else {
+    res.writeHead(404);
+    res.end("Page non trouvée");
+  }
+});
+
+server.listen(port, () => {
+  console.log("serveur démarre sur le port", port);
+});
