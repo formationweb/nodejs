15099b66e5b599fe0a8ba7106fd93ca8af040c5a
RSamaium <sam@newbot.io>
2024-09-25 15:55:34 +0200

    user model

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,86 +1,130 @@
-import { BadRequestError } from './../../errors/bad-request';
-import { NotFoundError } from './../../errors/not-found';
-import fs from 'fs'
-import { z, ZodError } from 'zod'
-import { Follow, followSchema, userSchema } from './users.schema';
-import { UserModel } from './users.model';
+import { BadRequestError } from "./../../errors/bad-request";
+import { NotFoundError } from "./../../errors/not-found";
+import fs from "fs";
+import { z, ZodError } from "zod";
+import { Follow, followSchema, userSchema, User } from "./users.schema";
+import { UserModel } from "./users.model";
 
-const data = JSON.parse(fs.readFileSync("src/data/posts.json", "utf-8"));
-const follows: Follow[] = []
+const follows: Follow[] = [];
 
-export async function getUsers(req, res ,next)  {
-    try {
-        const users = await UserModel.find()
-        res.json(users)
-    }
-    catch (err) {
-        next(err)
-    }
+/**
+ * UserModel.find()
+ * UserModel.findById(id)
+ * UserModel.findOne({ email: 'test@test.com' })
+ */
+
+export async function getUsers(req, res, next) {
+  try {
+    const users = await UserModel.find({
+        name: {
+            $eq: 'ana'
+        }
+    });
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
 }
 
-export function getUser(req, res ,next) {
-    const testValue = req.query.test
-    console.log(testValue)
-    const id = req.params.userId
-    res.json({
-        id, // équivalent à id: id
-        name: 'ana',
-        email: 'ana@gmail.com'
-    })
+export function getUser(req, res, next) {
+  const testValue = req.query.test;
+  console.log(testValue);
+  const id = req.params.userId;
+  res.json({
+    id, // équivalent à id: id
+    name: "ana",
+    email: "ana@gmail.com",
+  });
 }
 
 export function getUserPosts(req, res, next) {
-    const id = req.params.userId
-    const userPosts = data.filter(post => post.userId == id)
-    if (userPosts.length == 0) {
-        next(new NotFoundError('not user'))
-        return
-    }
-    res.json(userPosts)
+  const id = req.params.userId;
+  const userPosts = data.filter((post) => post.userId == id);
+  if (userPosts.length == 0) {
+    next(new NotFoundError("not user"));
+    return;
+  }
+  res.json(userPosts);
 }
 
-export function createUser(req, res, next) {
-    const { success, error, data } = userSchema.safeParse(req.body)
+export async function createUser(req, res, next) {
+ try {
+    const { success, error, data } = userSchema.safeParse(req.body);
     if (success) {
-        const { name, email } = data
-        res.status(201).json({ name, email })
+      const { name, email } = data;
+      const user = new UserModel({
+        name,
+        email,
+      });
+      const userCreated = await user.save();
+      res.status(201).json(userCreated);
+    } else {
+      throw new BadRequestError()
     }
-    else {
-        console.log(error.errors)
-        next(new BadRequestError())
-    }
+ }
+ catch (err) {
+    next(err)
+ }
 }
 
-export function updateUser(req, res, next) {
-    const id = req.params.userId
-    res.json({ name: 'test' })
+export async function updateUser(req, res, next) {
+  try {
+    const id = req.params.userId;
+    const { name, email } = req.body;
+    // const user = await UserModel.findById(id)
+    const user = await UserModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+      },
+      {
+        new: true,
+      }
+    );
+    if (!user) {
+      throw new NotFoundError("User");
+    }
+    // user.name = name
+    // await user.save()
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
 }
 
-export function deleteUser(req, res, next) {
-    const id = req.params.userId
-    res.status(204).send()
+export async function deleteUser(req, res, next) {
+  try {
+    const id = req.params.userId;
+    const userIdDeleted = await UserModel.findByIdAndDelete(id);
+    if (!userIdDeleted) {
+      throw new NotFoundError("User");
+    }
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
 }
 
 export function followUser(req, res, next) {
-   try {
-    const followData = followSchema.parse(req.body)
-    const { followerId, followeeId } = followData
+  try {
+    const followData = followSchema.parse(req.body);
+    const { followerId, followeeId } = followData;
 
-    if (!users.find(user => user.id == followerId)) {
-        throw new NotFoundError('User Id')
+    if (!users.find((user) => user.id == followerId)) {
+      throw new NotFoundError("User Id");
     }
-    if (!users.find(user => user.id == followeeId)) {
-        throw new NotFoundError('User Id')
+    if (!users.find((user) => user.id == followeeId)) {
+      throw new NotFoundError("User Id");
     }
 
-    follows.push(followData)
-    res.status(204).send()
-   }
-   catch (err) {
-     if (err instanceof ZodError) {
-        next(new BadRequestError(err.message))
-        return
-     }
-     next(err)
-   }
+    follows.push(followData);
+    res.status(204).send();
+  } catch (err) {
+    if (err instanceof ZodError) {
+      next(new BadRequestError(err.message));
+      return;
+    }
+    next(err);
+  }
 }
--- src/api/users/users.model.ts
+++ src/api/users/users.model.ts
@@ -1,6 +1,22 @@
 import { model, Schema } from "mongoose";
 
 export const UserModel = model('User', new Schema({
-    name: String,
-    email: String
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'member'],
+        default: 'member'
+    }
 }))
