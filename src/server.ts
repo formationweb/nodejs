d1b55908675c2de38a22c25d5d9c8f5eb793371c
RSamaium <sam@newbot.io>
2024-09-26 15:18:23 +0200

    pug

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,33 @@
 import 'dotenv/config'
 import express from 'express'
+import path from 'path'
 import usersRouter from './api/users/users.router'
 import postsRouter from './api/posts/posts.router'
 import meRouter from './api/me/me.router'
 import { NotFoundError } from './errors/not-found'
 import { authMiddleware } from './middlewares/auth'
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 app.use(express.json())
 
 app.use('/api/me', authMiddleware, meRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/posts', postsRouter)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/tpl', (req, res) => {
+    res.render('index', {
+        title: 'Ma Page',
+        message: 'Titre'
+    })
+})
 
 app.use((req, res, next) => {
     next(new NotFoundError('Page'))
