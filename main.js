e3f2133d589ebdd0297d9c2b15cb90783fea17c5
RSamaium <sam@newbot.io>
2024-12-17 14:46:23 +0100

    promise

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 import http from 'http'
-import fs from 'fs'
+import fs from 'node:fs/promises'
 import path from 'path'
 
-const port = 3000
+const port = process.argv[2] || 3000
 const dirname = import.meta.dirname // le dossier où se trouve main.js
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     if (req.url == '/') {
-        fs.readFile(path.join(dirname, 'static', 'index.html'), 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(500)
-                res.end(err.message)
-                return
-            }
+        // fs.readFile(path.join(dirname, 'static', 'index.html'), 'utf8', (err, data) => {
+        //     if (err) {
+        //         res.writeHead(500)
+        //         res.end(err.message)
+        //         return
+        //     }
+        //     res.writeHead(200, {
+        //         'Content-Type': 'text/html'
+        //     })
+        //     res.end(data)
+        // })
+        try {
+            const data = await fs.readFile(path.join(dirname, 'static', 'index.html'), 'utf8')
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             })
             res.end(data)
-        })
+        }
+        catch (err) {
+            res.writeHead(500)
+            res.end(err.message)
+        }
     }
     else {
         res.writeHead(404)
