f147ff4f12bb2dae6d4e8f8e85fb0ef507f7eeed
RSamaium <sam@newbot.io>
2024-12-17 12:28:44 +0100

    exo

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 import http from 'http'
+import fs from 'fs'
+import path from 'path'
 
 const port = 3000
+const dirname = import.meta.dirname // le dossier où se trouve main.js
 
 const server = http.createServer((req, res) => {
-    console.log(
-        req.headers,
-        req.method,
-        req.url
-    )
-    
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    res.end('<h1>Hello World</h1>')
+    if (req.url == '/') {
+        fs.readFile(path.join(dirname, 'static', 'index.html'), 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500)
+                res.end(err.message)
+                return
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(data)
+        })
+    }
+    else {
+        res.writeHead(404)
+        res.end('Page non trouvée')
+    }
 })
 
 server.listen(port, () => {
     console.log('serveur démarre sur le port', port)
 })
+
+
