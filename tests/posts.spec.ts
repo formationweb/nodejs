dc31aa1f6c6ef678d9f8f6aace646d5bbbd09c2a
RSamaium <sam@newbot.io>
2024-12-20 10:17:51 +0100

    posts spec

--- tests/posts.spec.ts
+++ tests/posts.spec.ts
@@ -1,10 +1,36 @@
 import request from 'supertest'
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import { app } from "../src/server";
+import mongoose, { mongo } from 'mongoose';
+import { Post } from '../src/api/posts/posts.model';
 
 const URL = '/api/posts'
 
 describe('Tester /api/posts', () => {
+    let post
+
+    beforeEach(async () => {
+        await mongoose.connection.dropDatabase();
+        await Post.syncIndexes()
+        post = new Post({
+            title: 'titre',
+            content: 'content',
+            userId: '66f513a415f3bdfd1b86658b'
+        });
+        await post.save();
+    });
+
+    // test('', async () => {
+    //     const indexes = await mongoose.connection.db?.collection('posts').indexes()
+    //     expect(indexes).toEqual(
+    //         expect.arrayContaining(
+    //             expect.objectContaining({
+    //                 key: { _fts: 'text',  _ftsx: 1 }
+    //             })
+    //         )
+    //     )
+    // })
+
     test('[GET] Posts', async () => {
         const res = await request(app).get(URL)
         expect(res.status).toBe(200)
@@ -12,20 +38,20 @@ describe('Tester /api/posts', () => {
     })
 
     test('[GET] Search Posts', async () => {
-        const res = await request(app).get(URL + '?search=sunt')
+        const res = await request(app).get(URL + '?search=titre')
         expect(res.status).toBe(200)
         expect(res.body.length).toBeGreaterThan(0)
     })
-
-    test('[GET] Post', async () => {
-        const res = await request(app).get(URL + '/1')
+    
+    test('[GET] Post Id', async () => {
+        const res = await request(app).get(URL + '/' + post._id)
         expect(res.status).toBe(200)
-        expect(res.body).toHaveProperty('id', 1)
+        expect(res.body).toHaveProperty('_id')
         expect(res.body).toHaveProperty('title')
     })
 
     test('[GET] Post not found', async () => {
-        const res = await request(app).get(URL + '/100000')
+        const res = await request(app).get(URL + '/66f513a415f3bdfd1b86658f')
         expect(res.status).toBe(404)
     })
 
